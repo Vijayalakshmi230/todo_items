e1b4290a21ab78dbfad95163796fb80d98b950ec
Vijayalakshmi230 <vijayalakshmipunyala2304@gmail.com>
2023-05-27 10:41:43 +0530

    Inital commit is done to save the file structure

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const PORT = 3500
+const todo_items = require('./routes/todo_items')
+
+app.get('/',(request,response)=>{
+    response.send('Working')
+})
+
+app.use('/api/v1/todo_items', todo_items)
+app.listen(PORT, console.log(`Server runs at http://localhost:3500`))
---
+++ routes/todo_items.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(request,response) =>{
+    response.send(`List of all to-do items`)
+})
+module.exports = router
